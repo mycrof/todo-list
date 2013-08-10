799d667f898bed6310d429616b6af502e18ffafe
mycrof <mycrof@live.cl>
2013-08-10 18:26:23 -0400

    agregado 4

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('.result6').html(printT_6());
 	$('.result1').html(printT_2());
 	$('.result3').html(printT_3());
-    
+    $('.result4').html(printT_4());
 
 });
 
@@ -17,4 +17,8 @@ return "Hola Mundo !!";
 
 function printT_3(){
     return " 3X1=3 - 3X2=6 - 3X3=9 - 3X4=12 - 3X5=15 - 3X6=18 - 3X7=21 - 3x8=24 - 3x9=27 - 3x10=30";
+}
+
+function printT_4(){
+    return " 4X1=4 - 4X2=8 - 4X3=12 - 4X4=16 - 4X5=20 - 4X6=24 - 4X7=28 - 4x8=32 - 4x9=36 - 4x10=40";
 }
