c2ef5abed4b550032cd6a2657e8c66712512c12a
Rogelio Meza <rogelio.meza.t@gmail.com>
2013-08-10 16:17:17 -0400

    functions.js has new function

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
-    $('.result').html(printT_1());
+    $('.result').html(printT_2());
 
 });
 
 function printT_1(){
     return "Hola Mundo!";
+}
+
+printT_2 = function(){
+    return "Hola Mundo 2!!";
 }
