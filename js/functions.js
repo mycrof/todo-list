4d4e6a519ad57a2f368d17b431a752c708c73151
mycrof <mycrof@live.cl>
2013-08-10 17:03:44 -0400

    arreglada la funcion  6 y el html

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 
-    $('.result').html(printT_1());
     $('.result6').html(printT_6());
+	$('.result1').html(printT_2());
+    
 
 });
 
 function printT_6(){
-    return "6 X 1 =6  6 X 1 =6  6 X 2 =12  6 X 3 =18  6 X 4 = 24  6 X 5 = 30 6 X 1 =36 6 X 7 =42 6 X 8 =48 6 X 9 =54 6 X 10 =60;
+    return "6 X 1 =6 6 X 1 =6 6 X 2 =12 6 X 3 =18 6 X 4 = 24 6 X 5 = 30 6 X 1 =36 6 X 7 =42 6 X 8 =48 6 X 9 =54 6 X 10 =60";
 }
 
 printT_2 = function(){
-    return "Hola Mundo 2!!";
+return "Hola Mundo 2!!";
 }
+
