c52ebd4875318f75dbaed0fd2438465986917b37
mycrof <mycrof@live.cl>
2013-08-10 13:36:22 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-    $('.result').html(printT_2());
+    $('.result').html(printT_1());
 
 });
 
-function printT_1(){
-    return "Hola Mundo!";
+function printT_6(){
+    return "6 X 1 =6  6 X 1 =6 6 X 2 =12 6 X 3 =18 6 X 4 = 24  6 X 5 = 30 6 X 1 =36 6 X 7 =42 6 X 8 =48 6 X 9 =54 6 X 10 =60;
 }
 
 printT_2 = function(){
