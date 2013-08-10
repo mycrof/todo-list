9631a7de25094e6addb7f369fd820837e4a5f279
mycrof <mycrof@live.cl>
2013-08-10 18:23:20 -0400

    agregada tabla 3

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
     $('.result6').html(printT_6());
 	$('.result1').html(printT_2());
+	$('.result3').html(printT_3());
     
 
 });
@@ -14,3 +15,6 @@ printT_2 = function(){
 return "Hola Mundo !!";
 }
 
+function printT_3(){
+    return " 3X1=3 - 3X2=6 - 3X3=9 - 3X4=12 - 3X5=15 - 3X6=18 - 3X7=21 - 3x8=24 - 3x9=27 - 3x10=30";
+}
