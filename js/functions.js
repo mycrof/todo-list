2ee27bda9e475a476aafa291500786494c7f5bc7
manloyola <manuel.loyola_v@hotmail.com>
2013-08-10 18:25:48 -0400

    cambio2

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,16 @@ $(document).ready(function(){
 });
 
 function printT_6(){
-    return " 2 X 1 =2 2 X 2 =4 2 X 3 =6 2 X 4 = 8 2 X 5 = 10 2 X 6 =12 2 X 7 =14 2 X 8 =16 2 X 9 =18 2 X 10 =20";
+    return " 2 X 1 =2;
+	         2 X 2 =4;
+			 2 X 3 =6;
+			 2 X 4 =8;
+			 2 X 5 =10;
+			 2 X 6 =12;
+			 2 X 7 =14;
+			 2 X 8 =16;
+			 2 X 9 =18;
+			 2 X 10 =20";
 }
 
 printT_2 = function(){
