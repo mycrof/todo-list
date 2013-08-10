ce2627429f545139c82742b182950b51fd109349
manloyola <manuel.loyola_v@hotmail.com>
2013-08-10 18:45:45 -0400

    cambio

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,13 @@
 $(document).ready(function(){
 
-    $('.result6').html(printT_6());
-	$('.result1').html(printT_2());
+    $('.result6').html(printT_2());
+	$('.result1').html(printT_6());
     
 
 });
 
-function printT_6(){
-    return " 2 X 1 =2;
-	         2 X 2 =4;
-			 2 X 3 =6;
-			 2 X 4 =8;
-			 2 X 5 =10;
-			 2 X 6 =12;
-			 2 X 7 =14;
-			 2 X 8 =16;
-			 2 X 9 =18;
-			 2 X 10 =20";
+function printT_2(){
+    return " 2 X 1 =2 2 X 2 =4 2 X 3 =6 2 X 4 =8 2 X 5 =10 2 X 6 =12 2 X 7 =14 2 X 8 =16 2 X 9 =18 2 X 10 =20";
 }
 
 printT_2 = function(){
