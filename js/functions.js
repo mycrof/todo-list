d1eb7187a939d223eebb724e52812ba819bae15c
juguerra <juangerra15@gmail.com>
2013-08-10 16:40:38 -0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 });
 
 function printT_6(){
-    return "6 X 1 =6  6 X 1 =6 6 X 2 =12 6 X 3 =18 6 X 4 = 24  6 X 5 = 30 6 X 1 =36 6 X 7 =42 6 X 8 =48 6 X 9 =54 6 X 10 =60;
+    return "6 X 1 =6  6 X 1 =6  6 X 2 =12  6 X 3 =18  6 X 4 = 24  6 X 5 = 30 6 X 1 =36 6 X 7 =42 6 X 8 =48 6 X 9 =54 6 X 10 =60;
 }
 
 printT_2 = function(){
