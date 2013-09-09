3ad4e87995ad41f4232f1c16df68f7c9447e37fe
jonathan <joninavarro@outlook.com>
2013-09-08 21:14:02 -0400

    evento estado de la tarea

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.result3').html(printT_3());
+    
+
+});
+
+function printT_3(){
+    return " 3X1=3 - 3X2=6 - 3X3=9 - 3X4=12 - 3X5=15 - 3X6=18 - 3X7=21 - 3x8=24 - 3x9=27 - 3x10=30";
+}
+
